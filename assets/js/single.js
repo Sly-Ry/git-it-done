2caef46dd8d8ce7d0daf1ef1625b70c836c8c59d
ryan <ryan.williams93@aol.com>
2021-12-08 16:54:02 -0600

    error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,10 +3,22 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function() {
+    
+    // grab repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+    
+    if (repoName) {
+        
+        // display repo name on page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        
+        // if no repo was given, redirect to homepage
+        document.location.replace("./index.html");
+    }
 };
 
 var getRepoIssues = function(repo) {
@@ -30,10 +42,10 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
-    console.log(repo);
 };
 
 var displayIssues = function(issues) {
