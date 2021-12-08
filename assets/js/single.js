87813f8a740fedcd6f57442ac1a4b0c9368b7bb7
ryan <ryan.williams93@aol.com>
2021-12-08 16:37:08 -0600

    linked html pages using quiery parameters

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,13 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+};
 
 var getRepoIssues = function(repo) {
     // the ?direction=asc option reverses order to return oldest->newest, otherwise the order will be newest->oldest
@@ -82,4 +90,5 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+
+getRepoName();
